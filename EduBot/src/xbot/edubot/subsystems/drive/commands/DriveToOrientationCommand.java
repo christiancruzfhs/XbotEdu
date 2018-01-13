dd96c005273e05f9db174355c0d81f5ba86e45ef
christiancruzfhs <christian.c1@live.com>
2018-01-13 15:31:34 -0800

    to orientation

--- EduBot/src/xbot/edubot/subsystems/drive/commands/DriveToOrientationCommand.java
+++ EduBot/src/xbot/edubot/subsystems/drive/commands/DriveToOrientationCommand.java
@@ -8,6 +8,10 @@ import xbot.edubot.subsystems.drive.DriveSubsystem;
 public class DriveToOrientationCommand extends BaseCommand{
 	
 	DriveSubsystem drive;
+	double target;
+	double initial;
+	double error;
+	boolean done;
 	
 	@Inject
 	public DriveToOrientationCommand(DriveSubsystem driveSubsystem) {
@@ -17,11 +21,19 @@ public class DriveToOrientationCommand extends BaseCommand{
 	public void setTargetHeading(double heading) {
 		// This method will be called by the test, and will give you a goal heading.
 		// You'll need to remember this target position and use it in your calculations.
+		target = heading;
 	}
 	
 	@Override
 	public void initialize() {
-		// If you have some one-time setup, do it here.		
+		// If you have some one-time setup, do it here.
+		initial = drive.gyro.getYaw();
+		while (target > 180) {
+			target -= 360;
+		}
+		while (target < -180) {
+			target += 360;
+		}
 	}
 
 	@Override
@@ -31,12 +43,25 @@ public class DriveToOrientationCommand extends BaseCommand{
 		// - Gets the robot stop (or at least be moving really really slowly) at the target position
 		
 		// How you do this is up to you. If you get stuck, ask a mentor or student for some hints!
+		double position = drive.gyro.getYaw();
+		double oldError = error;
+		error = Math.abs(target - position);
+		
+		double power = .055 * error - 0.5 * (oldError - error);
+		
+		drive.tankDrive(-power, power);
+		
+		oldError = error;
+		
+		if ((error > -.1 && error < .1)) {
+			done = true;
+		}
 	}
 
 	@Override
 	public boolean isFinished() {
 		// Modify this to return true once you have met your goal, 
 		// and you're moving fairly slowly (ideally stopped)
-		return false;
+		return done;
 	}
 }
