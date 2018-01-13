fc07a4efd658a57f5b7c220ce8e11c966c071a52
christiancruzfhs <christian.c1@live.com>
2018-01-13 15:23:25 -0800

    turn left 90

--- EduBot/src/xbot/edubot/subsystems/drive/commands/TurnLeft90DegreesCommand.java
+++ EduBot/src/xbot/edubot/subsystems/drive/commands/TurnLeft90DegreesCommand.java
@@ -8,6 +8,11 @@ import xbot.edubot.subsystems.drive.DriveSubsystem;
 public class TurnLeft90DegreesCommand extends BaseCommand {
 	
 	DriveSubsystem drive;
+	double position;
+	double initial;
+	double goal;
+	double error;
+	boolean done;
 	
 	@Inject
 	public TurnLeft90DegreesCommand(DriveSubsystem driveSubsystem) {
@@ -17,14 +22,34 @@ public class TurnLeft90DegreesCommand extends BaseCommand {
 	@Override
 	public void initialize() {
 		// TODO Auto-generated method stub
-		
+		initial = drive.gyro.getYaw();
+		goal = initial + 90;
+		if (goal > 180) {
+			goal -= 360;
+		}
 	}
 
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
+		position = drive.gyro.getYaw();
+		double oldError = error;
+		error = Math.abs(goal - position);
+		
+		double power = .055 * error - 0.5 * (oldError - error);
+		
+		drive.tankDrive(-power, power);
+		
+		oldError = error;
+		
+		if ((error > -.1 && error < .1) /*&& (power < 0.1 && power > -0.1)*/) {
+			done = true;
+		}
 	}
 
-
+	@Override
+	public boolean isFinished() {
+		return done;
+	}
 
 }
